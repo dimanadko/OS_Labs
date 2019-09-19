329b816aca04af6568d3cfed62577ef3279836da
@dimanadko <dimanadobko@gmail.com>
2019-09-19 09:28:51 +0300

    implement mem_free

--- memory_allocation/main.cpp
+++ memory_allocation/main.cpp
@@ -49,13 +49,17 @@ void *mem_alloc(size_t size){
 //    cout << "------" << endl << curr_pointer->free << endl;
 //    cout << curr_pointer->size << endl;
 //    cout << curr_pointer->prev_size << endl;
+//    cout << "=======" << curr_pointer->size << " - " << size << endl;
+//    cout << (curr_pointer->size < size) << endl;
+    cout << "new while" << endl;
 
-    while(!curr_pointer->free){
-//        cout << curr_pointer->free << endl;
-//
+    while((!curr_pointer->free) && (curr_pointer->size <= size)){
+        cout << "free: " << curr_pointer->free << ':' << curr_pointer->size << ':' << *(int*)(curr_pointer+HEADER_SIZE) << endl;
+//        cout << curr_pointer->size << " " << size << endl;
 
 //        *curr_pointer = currentHeader;
-        curr_pointer = curr_pointer + curr_pointer->size + HEADER_SIZE;
+        curr_pointer = (memory_block_header *)((char *)curr_pointer + curr_pointer->size + HEADER_SIZE);
+        cout << "free after: " << curr_pointer->free << endl;
     };
 
     struct memory_block_header nextHeader;
@@ -63,12 +67,12 @@ void *mem_alloc(size_t size){
     nextHeader.free = true;
     nextHeader.prev_size = size;
 
-    memory_block_header * next_pointer = curr_pointer + HEADER_SIZE + size;
+    memory_block_header * next_pointer = (memory_block_header *)((char *)curr_pointer + HEADER_SIZE + size);
     *next_pointer = nextHeader;
 
     *curr_pointer = currentHeader;
 
-    void * result_pointer = curr_pointer + HEADER_SIZE;
+    void * result_pointer = (memory_block_header *)((char *)curr_pointer + HEADER_SIZE);
     return result_pointer;
 
 //    memory_block_header * header_poiner = (memory_block_header *)(heap.addr);
@@ -85,8 +89,11 @@ void *mem_realloc(void *addr, size_t size){
     return heap.addr;
 };
 
-void *mem_free(void *addr){
-    return heap.addr;
+void mem_free(void *addr){
+    memory_block_header * curr_header = (memory_block_header *)((char*)addr-HEADER_SIZE);
+    cout<< (char*)(curr_header) + HEADER_SIZE << ":" << ((char*)(addr)-HEADER_SIZE)+HEADER_SIZE << " \n equal: " <<((char*)curr_header + HEADER_SIZE == ((char*)addr-HEADER_SIZE)+HEADER_SIZE )<< endl;
+    cout<< "delete:" << *(int*)(curr_header+HEADER_SIZE) << *(int*)addr <<endl;
+    curr_header->free = true;
 };
 
 
@@ -99,7 +106,12 @@ int main(int argc, const char * argv[]) {
     *a = 1;
     *b = 2;
     *c = 3;
+    mem_free(b);
+    memory_block_header* prev_b_header = (memory_block_header*)((char*)b-HEADER_SIZE);
+    cout << "prev_b_header: " << prev_b_header->free << endl;
+    int* d = (int *)mem_alloc(sizeof(int));
+    *d = 4;
     cout << "Hello, World!\n"<<endl;
-    cout << *a << *b << *c <<endl;
+    cout << *a << *b << *c << *d <<endl;
     return 0;
 }
